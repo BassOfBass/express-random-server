d31342a5dce7c8cd0e1b0a3f11565049f628698d
BoB <vinniethepooloo@protonmail.com>
2020-11-07 11:29:53 +0500

    form validation is on the way

---
+++ public/js/assess/form-validation.js
@@ -0,0 +1,15 @@
+(() =>  {
+  /** @type HTMLFormElement */
+  const validForm = document.querySelector("#jsvalidation");
+
+  validForm.addEventListener("submit", handleSubmission);
+
+  /**
+   * 
+   * @param {Event} e 
+   */
+  function handleSubmission(e) {
+
+  };
+  
+})();
